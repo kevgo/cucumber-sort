e5be3eb9269723415b624007375400df172e20d0
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-24 15:01:14 -0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,13 @@ fn main() -> ExitCode {
 fn inner() -> Result<usize> {
     let config = config::load()?;
     let mut issues = Vec::<Issue>::new();
-    for file in find::all()? {
-        let file = fs::File::open(&file).map_err(|e| UserError::CannotReadFile {
-            filename: file,
+    for filepath in find::all()? {
+        let file_content = fs::File::open(&filepath).map_err(|e| UserError::CannotReadFile {
+            filename: filepath,
             reason: e.to_string(),
         })?;
-        // let gherkin = gherkin::file(BufReader::new(file))?;
-        // sort::file(gherkin, &config, &mut issues);
+        let gherkin = gherkin::file(BufReader::new(file_content))?;
+        let sorted_file = sort::file(gherkin, &config, &mut issues);
     }
     for issue in &issues {
         println!("{}:{}  {}", issue.file, issue.line, issue.problem);
