f783da7c4476c9ee85e7d45e23ec9232d6bfe637
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-24 09:29:01 -0400

    End-to-end test framework (#1)

---
+++ tests/cuke.rs
@@ -0,0 +1,26 @@
+use cucumber::{World, then, when};
+use std::process::Command;
+
+#[derive(Debug, Default, World)]
+pub struct MyWorld {
+    output: String,
+}
+
+#[when("I run cucumber-sort")]
+async fn run_binary(world: &mut MyWorld) {
+    let output = Command::new("cargo")
+        .args(&["run", "--bin", "cucumber-sort"])
+        .output()
+        .unwrap();
+    world.output = String::from_utf8(output.stdout).unwrap();
+}
+
+#[then(expr = "it prints {string}")]
+async fn check_output(world: &mut MyWorld, expected: String) {
+    assert_eq!(world.output.trim(), expected);
+}
+
+#[tokio::test]
+async fn run_cucumber_tests() {
+    MyWorld::run("features").await;
+}
