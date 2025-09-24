445800a6a50b74b99e79710867879ee921f26d59
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-24 09:11:17 -0500

    Async subshell in tests

--- tests/cuke.rs
+++ tests/cuke.rs
@@ -1,5 +1,5 @@
 use cucumber::{World, then, when};
-use std::process::Command;
+use tokio::process::Command;
 
 #[derive(Debug, Default, World)]
 pub struct MyWorld {
@@ -11,6 +11,7 @@ async fn run_binary(world: &mut MyWorld) {
     let output = Command::new("cargo")
         .args(&["run", "--bin", "cucumber-sort"])
         .output()
+        .await
         .unwrap();
     world.output = String::from_utf8(output.stdout).unwrap();
 }
