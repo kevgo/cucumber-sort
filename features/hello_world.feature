2cdd2eb301eb6e72c7802dd447ee092f734f98e8
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-24 07:31:25 -0500

    .git-town.toml Cargo.lock Cargo.toml hello_world.feature cucumber.rs

---
+++ features/hello_world.feature
@@ -0,0 +1,5 @@
+Feature: Hello World Binary
+
+  Scenario: Running the binary prints Hello, world!
+    When I run the cucumber-sort binary
+    Then it prints "Hello, world!"
---
+++ tests/cucumber.rs
@@ -0,0 +1,27 @@
+use cucumber::{World, then, when};
+use std::process::Command;
+
+#[derive(Debug, Default, World)]
+pub struct MyWorld {
+    output: String,
+}
+
+#[when("I run the cucumber-sort binary")]
+async fn run_binary(world: &mut MyWorld) {
+    let output = Command::new("cargo")
+        .args(&["run", "--bin", "cucumber-sort"])
+        .output()
+        .unwrap();
+
+    world.output = String::from_utf8(output.stdout).unwrap();
+}
+
+#[then(regex = r#"it prints "(.*)""#)]
+async fn check_output(world: &mut MyWorld, expected: String) {
+    assert_eq!(world.output.trim(), expected);
+}
+
+#[tokio::test]
+async fn run_cucumber_tests() {
+    MyWorld::run("features").await;
+}
