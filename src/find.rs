dccab915fb8008000114394f5ab6b07ba6f42099
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-27 16:43:31 -0500

    find.rs

--- src/find.rs
+++ src/find.rs
@@ -2,20 +2,19 @@ use crate::prelude::*;
 use camino::{Utf8Path, Utf8PathBuf};
 
 pub fn all() -> Result<Vec<Utf8PathBuf>> {
-  let mut result = Vec::<Utf8PathBuf>::new();
-  search_folder(".", &mut result)?;
-  Ok(result)
+  search_folder(".")
 }
 
-fn search_folder(dir: impl AsRef<Utf8Path>, files: &mut Vec<Utf8PathBuf>) -> Result<()> {
+fn search_folder(dir: impl AsRef<Utf8Path>) -> Result<Vec<Utf8PathBuf>> {
+  let mut result = vec![];
   for entry in dir.as_ref().read_dir_utf8().unwrap() {
     let entry = entry.unwrap();
     let entry_path = entry.path();
     if entry_path.is_dir() {
-      search_folder(entry_path, files)?;
+      result.extend(search_folder(entry_path)?);
     } else if entry_path.extension() == Some("feature") {
-      files.push(entry_path.strip_prefix(".").unwrap().to_path_buf());
+      result.push(entry_path.strip_prefix(".").unwrap().to_path_buf());
     }
   }
-  Ok(())
+  Ok(result)
 }
