f724045280edc1e5d5dc584743adb2379d0b2c04
Kevin Goslar <kevin.goslar@gmail.com>
2025-09-30 12:01:03 -0500

    check.rs

--- src/cmd/check.rs
+++ src/cmd/check.rs
@@ -1,4 +1,5 @@
 use crate::errors::{Result, sort_issues};
+use crate::gherkin::Sorter;
 use crate::{config, filesystem, gherkin};
 use camino::Utf8PathBuf;
 use std::process::ExitCode;
@@ -7,7 +8,7 @@ use std::process::ExitCode;
 pub fn check(filepath: Option<Utf8PathBuf>) -> Result<ExitCode> {
   let config = config::load()?;
   match filepath {
-    Some(filepath) => file(filepath, &config),
+    Some(filepath) => file(filepath, &config.sorter),
     None => all(&config),
   }
 }
@@ -15,7 +16,7 @@ pub fn check(filepath: Option<Utf8PathBuf>) -> Result<ExitCode> {
 /// checks all files in the current folder
 fn all(config: &config::Config) -> Result<ExitCode> {
   for filepath in filesystem::find_matching(&config.ignorer)? {
-    let exit_code = file(filepath, config)?;
+    let exit_code = file(filepath, &config.sorter)?;
     if exit_code != ExitCode::SUCCESS {
       return Ok(exit_code);
     }
@@ -24,9 +25,9 @@ fn all(config: &config::Config) -> Result<ExitCode> {
 }
 
 /// checks the file with the given path
-fn file(filepath: Utf8PathBuf, config: &config::Config) -> Result<ExitCode> {
+fn file(filepath: Utf8PathBuf, sorter: &Sorter) -> Result<ExitCode> {
   let gherkin = gherkin::load(&filepath)?;
-  let (sorted_file, mut issues) = config.sorter.sort_file(gherkin.clone(), &filepath);
+  let (sorted_file, mut issues) = sorter.sort_file(gherkin.clone(), &filepath);
   let sorted_lines = sorted_file.lines();
   let original_lines = gherkin.lines();
   issues.extend(original_lines.find_mismatching(&sorted_lines, &filepath));
