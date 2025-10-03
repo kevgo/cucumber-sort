faf9636c2236fe50629f6201e8873a21257da6cb
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-03 11:28:35 -0500

    textrun-shell.js

--- textrun-shell.js
+++ textrun-shell.js
@@ -1,5 +1,11 @@
+import * as path from "path"
+import * as url from "url"
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const cucumber_sort_path = path.join(__dirname, "target", "debug", "cucumber-sort")
+
 export default {
   binaries: {
-    "cucumber-sort": path.join(__dirname, "target", "debug", "cucumber-sort")
+    "cucumber-sort": cucumber_sort_path
   }
 }
