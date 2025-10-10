ffd69a2e007e327347ee81e1267bfa6281471e74
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-10 06:12:12 -0500

    sorter.rs

--- src/gherkin/sorter.rs
+++ src/gherkin/sorter.rs
@@ -304,6 +304,36 @@ mod tests {
       let want_steps = DeletableSteps(vec![Some(step_1), None, None, Some(step_3), None]);
       assert_eq!(want_steps, steps);
     }
+
+    #[test]
+    fn extract_multiple_step_types() {
+      let step_1 = Step {
+        line_no: 1,
+        indent: S("  "),
+        keyword: Keyword::Given,
+        title: S("step 1"),
+        additional_lines: vec![],
+      };
+      let step_2 = Step {
+        line_no: 2,
+        indent: S("  "),
+        keyword: Keyword::Given,
+        title: S("step 2"),
+        additional_lines: vec![],
+      };
+      let step_3 = Step {
+        line_no: 3,
+        indent: S("  "),
+        keyword: Keyword::Given,
+        title: S("step 3"),
+        additional_lines: vec![],
+      };
+      let mut steps = DeletableSteps::from(vec![step_1.clone(), step_2.clone(), step_3.clone()]);
+      let extracted = steps.extract(&Regex::new("step [23]").unwrap());
+      assert_eq!(vec![step_2, step_3], extracted);
+      let want_steps = DeletableSteps(vec![Some(step_1), None, None]);
+      assert_eq!(want_steps, steps);
+    }
   }
 
   #[test]
