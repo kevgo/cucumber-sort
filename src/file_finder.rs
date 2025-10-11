19c8072c36aa6f212724579ceee99c4a643805bf
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-11 06:41:51 -0500

    file_finder.rs

--- src/file_finder.rs
+++ src/file_finder.rs
@@ -19,8 +19,13 @@ impl FileFinder {
         dir: dir.as_ref().to_path_buf(),
         message: err.to_string(),
       })?
-      .map(|opt| opt.unwrap())
-      .collect();
+      .map(|opt| {
+        opt.map_err(|err| UserError::FileRead {
+          file: dir.as_ref().to_path_buf(),
+          reason: err.to_string(),
+        })
+      })
+      .collect::<Result<Vec<_>>>()?;
     let mut paths: Vec<&Utf8Path> = entries.iter().map(|entry| entry.path()).collect();
     paths.sort();
     for path in paths {
