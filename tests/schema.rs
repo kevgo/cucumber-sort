c83972e912edc5439ce59356df80f5b6689ae0ac
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-15 09:14:23 -0500

    schema.rs

--- tests/schema.rs
+++ tests/schema.rs
@@ -6,5 +6,5 @@ use std::path::Path;
 #[test]
 fn export_schema() {
   let schema = serde_json::to_string_pretty(&schema_for!(Config)).unwrap() + "\n";
-  fs::write(&Path::new("docs").join("schema.json"), schema).unwrap();
+  fs::write(Path::new("docs").join("schema.json"), schema).unwrap();
 }
